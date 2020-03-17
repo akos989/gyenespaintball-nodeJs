62577def5977bcec604cfc4f7e7bd4b8fdd9ac18
akos989 <morvaiakos1998@gmail.com>
2020-03-17 10:03:23 +0100

    setup node, basic error handling, install nodemon

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    res.status(200).json({
+        message: 'It works!'
+    });
+});
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
