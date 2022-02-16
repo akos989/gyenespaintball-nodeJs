99dd15f96fa0c861a7b59874b8571e453d713ae5
akos989 <morvaiakos1998@gmail.com>
2022-02-16 12:37:22 +0100

    logging with winston

---
+++ logger.js
@@ -0,0 +1,32 @@
+const winston = require('winston');
+
+const options = {
+    file: {
+        level: 'info',
+        filename: `logs/app1.log`,
+        handleExceptions: true,
+        json: true,
+        maxsize: 5242880, // 5MB
+        maxFiles: 5,
+        colorize: false,
+    },
+    console: {
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true,
+    },
+};
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.File(options.file)
+    ],
+    format: winston.format.combine(
+        winston.format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+        winston.format.align(),
+        winston.format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+    )
+});
+
+module.exports = logger;
